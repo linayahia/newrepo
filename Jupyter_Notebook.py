ea46cf3b19d3c72598365cd1af9d18969cad1c74
linayahia <158504382+linayahia@users.noreply.github.com>
2024-02-11 14:01:58 +0200

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives**
+# - List popular languages for Data Science
+# - List popular libraries for Data Science
+# - List popular tools for Data Science
+# - Provide examples for arithmetic expressions in Python
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. Java
+# 3. R
+# 4. C++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. NumPy
+# 3. TensorFlow
+# 4. MatplotLib
+
+# |Data Science Tools|
+# ------------------
+# |Jupyter|
+# |Apache|
+# |Zeppelin|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+
+# In[2]:
+
+
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+
+# In[4]:
+
+
+total_minutes= 200
+hours=total_minutes //60
+minutes=total_minutes %60
+time= "{}:{}".format(hours,minutes)
+print(time)
+
+
+# ## Author:
+# Lina Yahia
+
+# In[ ]:
+
+
+
+
